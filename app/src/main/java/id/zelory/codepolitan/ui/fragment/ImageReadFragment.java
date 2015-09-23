8e4fef00d9677578e82b34b273864b860aa7e27b
Zetra <zetra@mail.ugm.ac.id>
2015-09-23 18:37:36 +0700

    remove unused import

--- app/src/main/java/id/zelory/codepolitan/ui/fragment/ImageReadFragment.java
+++ app/src/main/java/id/zelory/codepolitan/ui/fragment/ImageReadFragment.java
@@ -29,9 +29,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
 import java.util.List;
 
